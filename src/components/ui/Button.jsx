8ab3e4591b09850c8fa265bb5043db6669933dad
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2025-09-02 18:15:43 +0500

    feat: ✨ Update Button component styles

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -8,7 +8,7 @@ export default function Button({ className, children, onClick = () => { }, ...pr
             onClick={onClick}
             className={clsx(
                 "px-4 py-2 rounded transition-all ease-in duration-300 focus:outline-none bg-green-100 font-ari font-semibold text-dark-800 cursor-pointer border border-4 border-green-100",
-                "hover:-translate-y-2 hover:shadow-[0_16px_0_0_#000] hover:shadow-black hover:bg-light hover:text-green-100",
+                // "hover:-translate-y-2 hover:shadow-[0_16px_0_0_#000] hover:shadow-black hover:bg-light hover:text-green-100",
                 "shadow-none",
                 className
             )}
