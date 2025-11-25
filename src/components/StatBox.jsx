c69c7284052f3665d151998069749c76c1ff0372
mariaasghar123 <mariaasghar74@gmail.com>
2025-11-25 14:29:28 +0500

    small change in statbox

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,6 +1,6 @@
 export default function StatBox({ icon: Icon, value, label }) {
     return (
-        <div className="border-[1px] border-border rounded-xl gap-4 px-3 py-2 md:max-w-[220px] w-full flex bg-[#98F08C]/50 dark:bg-dark-700" >
+        <div className="border-[1px] border-border rounded-xl gap-4 px-3 py-2 md:max-w-[220px] w-full flex sm:bg-gray-200 bg-[#98F08C]/50 dark:bg-dark-700" >
             <div className="bg-[#5DD075] dark:bg-[#B8FAE233] p-2 rounded-full">
                 <Icon className="w-7 h-7 text-white dark:text-green flex-shrink-0" />
             </div>
